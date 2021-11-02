231132ef0f0730887dcf3570a8fae49b6361a748
Eiko0705 <eiko.imai.070502@gmail.com>
2021-11-02 14:55:08 +0900

    feat: create basic frontend

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+from newsapi import NewsApiClient
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    name = "Eiko"
-    return render_template('index.html', title='flask test', name=name)
+    # initialize client
+    newsapi = NewsApiClient(api_key='37fc69d716d14e20a12378ea93681428')
+
+    # get the headline news from the site assigned at sources
+    headlines = newsapi.get_top_headlines(sources='techcrunch')
+
+    articles = headlines['articles']
+
+    content = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        content.append(myarticles['content'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, content, img)
+
+    return render_template('index.html', title='News Summary App', context=mylist)
 
 
 @app.route('/good')
